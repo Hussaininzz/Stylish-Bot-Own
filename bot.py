9b72977da5d1aeb4faec3a98a1b41462d5590af2
Hussaininzz <99072679+Hussaininzz@users.noreply.github.com>
2022-05-06 00:22:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import CallbackQuery
 
+
+Stylish=Client(
+    "Stylish Bot",
+    bot_token="5302212624:AAHlXG2vappLbK2gaKmD1wxJ4DJajGc6T5w",
+    api_hash="5820bc246505e0ff60af5391d649f9a6",
+    api_id="8406611"
+)
+
+
+@Stylish.on_message(filters.command("start"))
+async def start_message(bot, message):
+    await message.reply_text("Hi Man 😁")
+
+
+Stylish.run()
